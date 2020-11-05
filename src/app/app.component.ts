7afab79a9cc7185e9b40d9bda7fd254e9fc39caa
shyamala <shyama28.raji@gmail.com>
2020-11-05 14:55:56 +0530

    .

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,35 @@
-import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   page = 'category';
   title = 'library';
+  token;
 
-  constructor(private router: Router) {
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute) {
+
+  }
+
+  ngOnInit() {
+    this.router.events.subscribe(a => {
+      if (a instanceof NavigationEnd) {
+        this.token = localStorage.getItem('token');
+    }
+
+    });
   }
 
 
   logoutApplication(): void {
     localStorage.removeItem('token');
     this.router.navigate(['/']);
-
+    
   }
 }
